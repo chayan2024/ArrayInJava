c981a24e14a4c24665e4853a4e7a634bb9a6ed2a
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-15 18:52:17 +0530

    Creating and Initializing an Array

---
+++ CreatingAndInitializingArray.java
@@ -0,0 +1,10 @@
+public class CreatingAndInitializingArray {
+
+    public static void main(String[] args){
+        System.out.println("Creating and Initializing an Array");
+        int array[]={1,2,3,4,5,6,7,8,9,10};
+        for(int i=0;i<array.length;i++){
+            System.out.println("Values in Array:"+array[i]);
+        }
+    }
+}
