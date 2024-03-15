28725a8cbf573090bf080931ff6d194b26ad64dd
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-15 23:27:50 +0530

    Finding the Length of an Array

---
+++ FindingLengthArray.java
@@ -0,0 +1,9 @@
+public class FindingLengthArray {
+
+    public static void main(String[] args){
+
+        System.out.println("Finding the Length of an Array");
+        int array[]={10,20,30,40,50,60,70,80,90};
+        System.out.println("Finding the Length of an Array"+array.length);
+    }
+}
