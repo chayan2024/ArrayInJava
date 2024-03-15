bdae68759776eb3f10a33d9b042b22f2e674825e
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-15 23:31:39 +0530

    Looping through an Array

---
+++ LoopingThroughArray.java
@@ -0,0 +1,10 @@
+public class LoopingThroughArray {
+
+    public static void main(String[] args){
+        System.out.println("Looping through an Array");
+        int array[]={11,22,33,4,66,77,88,33,22,4444};
+        for (int i = 0; i < array.length ; i++) {
+            System.out.println(" Looping through an Array"+"index of"+"  " +i +"  :: "+array[i]);
+        }
+    }
+}
