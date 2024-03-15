c99631e45276dc40c248548722675b02fe3e5d88
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-15 18:59:48 +0530

    Accessing Elements of an Array

---
+++ AccessingElementsArray.java
@@ -0,0 +1,8 @@
+public class AccessingElementsArray {
+
+    public static void main(String[] args){
+        System.out.println("Accessing Elements of an Array");
+        int array[]={1,2,3,4,5,6,7,8,9};
+        System.out.println("Access Data:"+array[5]);
+    }
+}
