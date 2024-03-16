b3608025b52a586f525c814cdf264dd98aa93185
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-16 16:44:33 +0530

    Sorting Arrays

---
+++ SortingArrays.java
@@ -0,0 +1,8 @@
+public class SortingArrays {
+    public static void main(String[] args) {
+        System.out.println("Sorting Arrays");
+        int array[]={10,22,44,2,5,6,1,3,4};
+        java.util.Arrays.sort(array);
+        System.out.println("Sorting Arrays::"+java.util.Arrays.toString(array));
+    }
+}
