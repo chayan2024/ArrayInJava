ef1f9eb1c081d01fd4436d15da88ef222473fc46
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-17 16:17:56 +0530

    Copying Arrays in Java

--- CopyingArrays.java
+++ CopyingArrays.java
@@ -1,8 +1,15 @@
 public class CopyingArrays {
     public static void main(String[] args) {
         System.out.println("Copying Arrays");
-        int array[]={1,2,3,4,5};
+        int array[]={1,2,3,4};
         int newArray[]=java.util.Arrays.copyOf(array,array.length);
         System.out.println("Copying Arrays into new array::"+java.util.Arrays.toString(newArray));
+
+        // another approach
+
+        int b[]=new int[array.length];
+        b=array;
+        b[0]++;
+        System.out.println("Copy:;"+java.util.Arrays.toString(b));
     }
 }
